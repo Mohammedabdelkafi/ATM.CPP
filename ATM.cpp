4fb6be8365ef2c78ed9796e1e8f4e6eec770792e
Mohammed abdelkafi <kawruusir@gmail.com>
2025-02-25 23:31:23 +0100

    Update ATM.cpp

--- ATM.cpp
+++ ATM.cpp
@@ -1,5 +1,52 @@
 #include <stdio.h>
-int ID;
-char USER[];
-char PASSWORD[];
-int main() {}
+#include <stdlib.h>
+int SECRET_CODE;
+int choice;
+
+void clrscr()
+{
+    system("@cls||clear");
+}
+int userRegister() {
+	clrscr();
+	
+}
+int userLogin() {
+	clrscr();
+	
+}
+
+int main() {
+	printf("ATM \n");
+    printf("\n");
+	printf("\n");                    
+    printf("1) Register \n");
+    printf("2) Login \n");
+    printf("3) exit \n");
+    printf(" \n");
+    printf("====>");
+    scanf("%d", &choice);
+}
+int AccMenu() {
+	printf(" %d \n", SECRET_CODE);
+	printf("\n");
+	printf("\n");                    
+    printf("1) Register \n");
+    printf("2) Login \n");
+    printf("3) exit \n");
+    printf(" \n");
+    printf("====>");
+    scanf("%d", &choice);
+    switch (choice) {
+    	case 1:
+    		userRegister();
+    		break;
+    	case 2:
+    		userLogin();
+    		break;
+		case 3:
+			return 0;
+		default:
+			clrscr();	
+	}
+}
